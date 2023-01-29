e1edc2ef0ab68898186b76d9e4dd52daa238997b
Lewis Murch <87498439+lewisMurch@users.noreply.github.com>
2023-01-29 15:26:34 +0000

    Update data_representor.py

--- data_representor.py
+++ data_representor.py
@@ -8,24 +8,46 @@ data = [3, 5, 1, 7, 4, 6, 8, 2]
 
 def draw_plot():
     fig = Figure()
-    if var.get() == "Bar Chart":
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.get_tk_widget().pack_forget()
+    if show_bar.get():
         ax = fig.add_subplot(111)
         ax.bar(range(len(data)), data)
-    elif var.get() == "Pie Chart":
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    if show_pie.get():
         ax = fig.add_subplot(111, aspect="equal")
         ax.pie(data)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+def generate():
+    fig = Figure()
     canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    canvas.get_tk_widget().pack_forget()
+    if show_bar.get():
+        ax = fig.add_subplot(111)
+        ax.bar(range(len(data)), data)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    if show_pie.get():
+        ax = fig.add_subplot(111, aspect="equal")
+        ax.pie(data)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 root = tk.Tk()
-var = tk.StringVar(value="Bar Chart")
-rb1 = tk.Radiobutton(root, text="Bar Chart", variable=var, value="Bar Chart", command=draw_plot)
-rb2 = tk.Radiobutton(root, text="Pie Chart", variable=var, value="Pie Chart", command=draw_plot)
-btn = tk.Button(root, text="Draw", command=draw_plot)
 
-rb1.pack()
-rb2.pack()
-btn.pack()
+show_bar = tk.BooleanVar(value=True)
+cb1 = tk.Checkbutton(root, text="Bar Chart", variable=show_bar)
+
+show_pie = tk.BooleanVar(value=True)
+cb2 = tk.Checkbutton(root, text="Pie Chart", variable=show_pie)
+
+cb1.pack()
+cb2.pack()
+
+generate_button = tk.Button(root, text="Generate", command=generate)
+generate_button.pack()
 
 root.mainloop()
